f862bf511703039e2e47437b01f07f5f875a7398
Aditya Priyadarshi <adityapriyadarshi669@gmail.com>
2022-12-30 12:13:54 +0530

    I've did some questionable things

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
 fn main() {
-    let secret_num = rand::thread_rng().gen_range(1..=100);
-
-    println!("Number Guessing Game");
-    loop {
-        println!("Enter A number:");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Unable to read the stdin");
-        let guess: u32 = match guess.trim().parse(){
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        match guess.cmp(&secret_num) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too Big"),
-            Ordering::Equal => {
-                println!("You Won!");
-                break;
-            }
-        }
-    }
+    let mut x = 24;
+    let y =  &mut x;
+    println!("{x} : {y}");
 }
